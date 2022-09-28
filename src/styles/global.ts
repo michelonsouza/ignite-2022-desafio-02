ea038dab392c769b081375bd957294a54d8c6745
Michelon Souza <michelon.souza@hotmail.com>
2022-09-28 12:26:40 -0300

    initial commit

---
+++ src/styles/global.ts
@@ -0,0 +1,28 @@
+import { createGlobalStyle, css } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  :focus {
+    outline: 0;
+    box-shadow: 0 0 0 2px ${({ theme }) => theme.yellow.main};
+  }
+
+  body {
+    ${({ theme }) => css`
+      background: ${theme.background};
+      color: ${theme.typography.text};
+      -webkit-font-smoothing: antialiased;
+    `}
+  }
+  
+  body, input, button, textarea {
+    font-family: Roboto, sans-serif;
+    font-weight: 400;
+    font-size: 1rem;
+  }
+`;
---
+++ src/styles/themes/default.ts
@@ -0,0 +1,26 @@
+export const defaultTheme = {
+  background: '#FAFAFA',
+  white: '#FFFFFF',
+  yellow: {
+    main: '#DBAC2C',
+    dark: '#C47F17',
+    light: '#F1E9C9',
+  },
+  purple: {
+    main: '#8047F8',
+    dark: '#4B2995',
+    light: '#EBE5F9',
+  },
+  typography: {
+    title: '#272221',
+    subtitle: '#403937',
+    text: '#574F4D',
+    label: '#8D8686',
+  },
+  common: {
+    hover: '#D7D5D5',
+    button: '#E6E5E5',
+    input: '#EEEDED',
+    card: '#F3F2F2',
+  },
+};
